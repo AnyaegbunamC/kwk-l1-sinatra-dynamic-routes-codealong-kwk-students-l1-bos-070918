21fcc52d0099daf6e36ebf3b855be37377263059
Celine Anyaegbunam <cea03@icloud.com>
2018-07-17 18:43:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ get '/goodbye/:name' do
   "Goodbye #{@given_name}!"
 end 
 
-get '/multiply/:num1/ :num2'do 
+get '/multiply/:num1/:num2'do 
   @num1 = params[:num1].to_i 
   @num2 = params[:num2].to_i 
   answer = (@num1 * @num2)
